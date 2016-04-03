27c06433867cf7b91542e37cba00c30b99873580
Justyna Bielecka <justynbielecka@gmail.com>
2016-04-03 15:43:56 +0200

    Warsztat 2 chit on chair zrobiony

--- js/app.js
+++ js/app.js
@@ -5,11 +5,23 @@ document.addEventListener("DOMContentLoaded", function(){
   var btnNExt = document.getElementById("btnNext");
   var sliderPics = document.querySelectorAll(".img-list li");
   var counterVisiblePic = 0;
+  //variables from section application
   var listArrow = document.querySelectorAll(".list_arrow");
-
-
-
-
+  var listPanelLists = document.querySelectorAll(".list_panel li");
+  var listPanels = document.querySelectorAll(".list_panel");
+  var transportCheckBox = document.getElementById("transport");
+//variables from summary_panel
+  var title = document.querySelector(".title");
+  var color = document.querySelector(".color");
+  var pattern = document.querySelector(".pattern");
+  var transport = document.querySelector(".transport");
+  var titleValue = document.querySelector(".title.value");
+  var colorValue = document.querySelector(".color.value");
+  var patternValue = document.querySelector(".pattern.value");
+  var transportValue = document.querySelector(".transport.value");
+  var sumValue = document.querySelector(".sum");
+  //array suma for costs of chair type, color, pattern, transport
+  var suma = [0, 0, 0, 0];
 
   for(var i=0; i<menu.length; i++){
     menu[i].addEventListener("mouseover", function(event){
@@ -37,14 +49,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
   sliderPics[counterVisiblePic].style.display = "inline-block";
 
- btnPrev.addEventListener("click", function(event){
-   sliderPics[counterVisiblePic].style.display = "none";
-   counterVisiblePic--;
-   if(counterVisiblePic < 0) {
-     counterVisiblePic = sliderPics.length - 1;
-   }
-   sliderPics[counterVisiblePic].style.display = "inline-block";
- });
+  btnPrev.addEventListener("click", function(event){
+    sliderPics[counterVisiblePic].style.display = "none";
+    counterVisiblePic--;
+    if(counterVisiblePic < 0) {
+      counterVisiblePic = sliderPics.length - 1;
+    }
+    sliderPics[counterVisiblePic].style.display = "inline-block";
+  });
 
   btnNext.addEventListener("click", function(event){
     sliderPics[counterVisiblePic].style.display = "none";
@@ -63,7 +75,50 @@ document.addEventListener("DOMContentLoaded", function(){
       }else{
          listPanel.style.display = "none";
       }
-
     });
   }
+
+  //function which count total cost of each chair
+  function totalCost(array){
+    return array.reduce(function(a,b){
+        return a+b;
+    });
+  }
+
+  for(var j=0; j<listPanelLists.length; j++){
+    listPanelLists[j].addEventListener("click", function(event){
+      var listLabel = this.parentNode.previousElementSibling.previousElementSibling;
+      listLabel.innerHTML = this.innerText;
+      this.parentNode.style.display = "none";
+      if(this.parentNode === listPanels[0]){
+        title.innerHTML = this.innerText;
+        titleValue.innerHTML = this.dataset.price + " zł";
+        suma[0] =  parseInt(this.dataset.price);
+        var print = totalCost(suma);
+        sumValue.innerHTML = print + " zł";
+      }else if(this.parentNode === listPanels[1]){
+        color.innerHTML = this.innerText;
+        colorValue.innerHTML = this.dataset.price + " zł";
+        suma[1] =  parseInt(this.dataset.price);
+        var print = totalCost(suma);
+        sumValue.innerHTML = print + " zł";
+      }else{
+        pattern.innerHTML = this.innerText;
+        patternValue.innerHTML = this.dataset.price + " zł";
+        suma[2] =  parseInt(this.dataset.price);
+        var print = totalCost(suma);
+        sumValue.innerHTML = print + " zł";
+      }
+    });
+  }
+
+  transportCheckBox.addEventListener("change", function(event){
+    if(transportCheckBox.checked === true){
+      transport.innerHTML =  "Transport";
+      suma[3] = parseInt(this.dataset.transportPrice)
+      var print = totalCost(suma);
+      transportValue.innerHTML = this.dataset.transportPrice + " zł";
+      sumValue.innerHTML = print + " zł";
+    }
+  });
 });
